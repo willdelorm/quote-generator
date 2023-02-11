9718bfc796b2632d99b98252c2b413e5f2fa977e
Will Delorm <willdelorm@gmail.com>
2023-02-10 23:21:06 -0800

    add complete project

---
+++ quotes.js
@@ -0,0 +1,27 @@
+const localQuotes = [
+  {
+    "text": "Today is the tomorrow we worried about yesterday.",
+    "author": "Anonymous",
+    "tag": "general"
+  },
+  {
+    "text": "It's easier to see the mistakes on someone else's paper.",
+    "author": null,
+    "tag": "general"
+  },
+  {
+    "text": "Every man dies. Not every man really lives.",
+    "author": "Anonymous",
+    "tag": "general"
+  },
+  {
+    "text": "The day is already blessed, find peace within it.",
+    "author": "Anonymous",
+    "tag": "general"
+  },
+  {
+    "text": "From small beginnings come great things.",
+    "author": "Anonymous",
+    "tag": "general"
+  },
+];
---
+++ script.js
@@ -0,0 +1,69 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+function newQuote() {
+  loading();
+  //Pick a random quote
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  // Check if author field is blank
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check quote length to determine style
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// Get quotes from API
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch {
+    // Catch Error here
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuotes();
